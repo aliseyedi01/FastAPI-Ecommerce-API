84ced9b1e7379f008c524b5131b6a839590bf2b0
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-11-30 23:49:06 +0330

    Refactor: Remove Redundant Root Endpoint

--- app/main.py
+++ app/main.py
@@ -2,12 +2,6 @@ from app.routers import products, categories, carts
 from fastapi import FastAPI
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, FastAPI in app directory!"}
-
-
 app.include_router(products.router)
 app.include_router(categories.router)
 app.include_router(carts.router)
