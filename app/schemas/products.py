82a837dbcdb1257c7533b14d99865330991b7e3e
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-12-01 18:56:11 +0330

    feat (schema) : add validation for discount percentage in products model

--- app/schemas/products.py
+++ app/schemas/products.py
@@ -1,9 +1,10 @@
 from datetime import datetime
 from typing import List, Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
+# Base Models
 class CategoryBase(BaseModel):
     id: int
     name: str
@@ -33,18 +34,37 @@ class ProductBase(BaseModel):
         pass
 
 
-class ProductCreate(ProductBase):
-    id: int = Field(exclude=True)
-    category: CategoryBase = Field(exclude=True)
+# Create Product
+class ProductCreate(BaseModel):
+    title: str
+    description: Optional[str]
+    price: int
+
+    @validator("discount_percentage", pre=True)
+    def validate_discount_percentage(cls, v):
+        if v < 0 or v > 100:
+            raise ValueError("discount_percentage must be between 0 and 100")
+        return v
+    discount_percentage: float
+    rating: float
+    stock: int
+    brand: str
+    thumbnail: str
+    images: List[str]
+    is_published: bool
+    created_at: datetime
+    category_id: int
 
     class Config(BaseConfig):
         pass
 
 
+# Update Product
 class ProductUpdate(ProductCreate):
     pass
 
 
+# Get Products
 class ProductOut(BaseModel):
     message: str
     data: ProductBase
@@ -61,8 +81,21 @@ class ProductsOut(BaseModel):
         pass
 
 
-class ProductDelete(ProductBase):
-    category: CategoryBase = Field(exclude=True)
+# Delete Product
+class ProductDelete(BaseModel):
+    id: int
+    title: str
+    description: Optional[str]
+    price: int
+    discount_percentage: float
+    rating: float
+    stock: int
+    brand: str
+    thumbnail: str
+    images: List[str]
+    is_published: bool
+    created_at: datetime
+    category_id: int
 
 
 class ProductOutDelete(BaseModel):
