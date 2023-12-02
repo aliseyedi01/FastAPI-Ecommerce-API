eb5de9f00aca88d8efba52a4a99a946c79f030ef
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-12-02 16:08:27 +0330

    feat (users)  : add description for search parameter

--- app/routers/users.py
+++ app/routers/users.py
@@ -14,7 +14,7 @@ def get_all_users(
     db: Session = Depends(get_db),
     page: int = Query(1, ge=1, description="Page number"),
     limit: int = Query(10, ge=1, le=100, description="Items per page"),
-    search: Optional[str] = "",
+    search: Optional[str] = Query("", description="Search based username"),
     role: str = Query("user", enum=["user", "admin"])
 ):
     return UserService.get_all_users(db, page, limit, search, role)
