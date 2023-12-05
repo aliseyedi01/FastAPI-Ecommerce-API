6772e5357d4873ad9d722a7e0ba0e9a7529e1177
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-12-05 20:57:48 +0330

    feat (categories) : add role based permissions for categories endpoint

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -3,12 +3,17 @@ from app.db.database import get_db
 from app.services.categories import CategoryService
 from sqlalchemy.orm import Session
 from app.schemas.categories import CategoryCreate, CategoryOut, CategoriesOut, CategoryOutDelete, CategoryUpdate
+from app.core.security import check_admin_role
+
 
 router = APIRouter(tags=["Categories"], prefix="/categories")
 
 
 # Get All Categories
-@router.get("/", status_code=status.HTTP_200_OK, response_model=CategoriesOut)
+@router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=CategoriesOut)
 def get_all_categories(
     db: Session = Depends(get_db),
     page: int = Query(1, ge=1, description="Page number"),
@@ -19,24 +24,39 @@ def get_all_categories(
 
 
 # Get Category By ID
-@router.get("/{category_id}", status_code=status.HTTP_200_OK, response_model=CategoryOut)
+@router.get(
+    "/{category_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=CategoryOut)
 def get_category(category_id: int, db: Session = Depends(get_db)):
     return CategoryService.get_category(db, category_id)
 
 
 # Create New Category
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=CategoryOut)
+@router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=CategoryOut,
+    dependencies=[Depends(check_admin_role)])
 def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
     return CategoryService.create_category(db, category)
 
 
 # Update Existing Category
-@router.put("/{category_id}", status_code=status.HTTP_200_OK, response_model=CategoryOut)
+@router.put(
+    "/{category_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=CategoryOut,
+    dependencies=[Depends(check_admin_role)])
 def update_category(category_id: int, updated_category: CategoryUpdate, db: Session = Depends(get_db)):
     return CategoryService.update_category(db, category_id, updated_category)
 
 
 # Delete Category By ID
-@router.delete("/{category_id}", status_code=status.HTTP_200_OK, response_model=CategoryOutDelete)
+@router.delete(
+    "/{category_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=CategoryOutDelete,
+    dependencies=[Depends(check_admin_role)])
 def delete_category(category_id: int, db: Session = Depends(get_db)):
     return CategoryService.delete_category(db, category_id)
