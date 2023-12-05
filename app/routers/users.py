81dbc6a6c828792c3eb4a34e4abf337ce30ee2d8
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-12-05 21:13:49 +0330

    feat (user) : add role base permission to user endpoints

--- app/routers/users.py
+++ app/routers/users.py
@@ -3,12 +3,18 @@ from app.db.database import get_db
 from app.services.users import UserService
 from sqlalchemy.orm import Session
 from app.schemas.users import UserCreate, UserOut, UsersOut, UserOutDelete, UserUpdate
+from app.core.security import check_admin_role
+
 
 router = APIRouter(tags=["Users"], prefix="/users")
 
 
 # Get All Users
-@router.get("/", status_code=status.HTTP_200_OK, response_model=UsersOut)
+@router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=UsersOut,
+    dependencies=[Depends(check_admin_role)])
 def get_all_users(
     db: Session = Depends(get_db),
     page: int = Query(1, ge=1, description="Page number"),
@@ -20,24 +26,40 @@ def get_all_users(
 
 
 # Get User By ID
-@router.get("/{user_id}", status_code=status.HTTP_200_OK, response_model=UserOut)
+@router.get(
+    "/{user_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=UserOut,
+    dependencies=[Depends(check_admin_role)])
 def get_user(user_id: int, db: Session = Depends(get_db)):
     return UserService.get_user(db, user_id)
 
 
 # Create New User
-@router.post("/", status_code=status.HTTP_201_CREATED, response_model=UserOut)
+@router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserOut,
+    dependencies=[Depends(check_admin_role)])
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return UserService.create_user(db, user)
 
 
 # Update Existing User
-@router.put("/{user_id}", status_code=status.HTTP_200_OK, response_model=UserOut)
+@router.put(
+    "/{user_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=UserOut,
+    dependencies=[Depends(check_admin_role)])
 def update_user(user_id: int, updated_user: UserUpdate, db: Session = Depends(get_db)):
     return UserService.update_user(db, user_id, updated_user)
 
 
 # Delete User By ID
-@router.delete("/{user_id}", status_code=status.HTTP_200_OK, response_model=UserOutDelete)
+@router.delete(
+    "/{user_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=UserOutDelete,
+    dependencies=[Depends(check_admin_role)])
 def delete_user(user_id: int, db: Session = Depends(get_db)):
     return UserService.delete_user(db, user_id)
